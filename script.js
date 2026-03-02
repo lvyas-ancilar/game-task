969396a23d3d1a7fa0f0bb097ce0c44ef190b363
Lavesh Vyas <lvyas@ancilar.com>
2026-03-02 11:16:27 +0530

    Add startGame function and defer listeners/intervals

--- script.js
+++ script.js
@@ -1,94 +1,48 @@
-// const dino = document.getElementById("dino");
-// const cactus = document.getElementById("cactus");
-
-// function jump() {
-//   if (dino.classList != "jump") {
-//     dino.classList.add("jump");
-
-//     setTimeout(function () {
-//       dino.classList.remove("jump");
-//     }, 3000);
-//   }
-// }
-
-// let isAlive = setInterval(function () {
-  
-//   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-//   console.log(dinoTop);   
-
-  
-//   let cactusLeft = parseInt(
-//     window.getComputedStyle(cactus).getPropertyValue("left")
-    
-//   );
-//   console.log(cactusLeft)
-
-  
-//   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-   
-//     alert("Game Over!");
-//   }
-// }, 10);
-
-// document.addEventListener("keydown", function (event) {
-//   jump();
-// });
-
-
-// let score = 0;
-// let scoreInterval = setInterval(function() {
-//   score++;
-//   document.getElementById("score").innerText = "Score: " + score;
-// }, 1000);
-
-
-// // function resetScore() {
-// //   score = 0;
-// //   document.getElementById("score").innerText = "Score: " + score;
-// // }
-
-// // document.addEventListener("keydown", function(event) {
-// //   
-// // }); 
+// Game elements
 var character = document.getElementById("character");
 var result = document.getElementById("result");
 var game = document.getElementById("game");
 var score = document.getElementById("score");
-
+var startBtn = document.getElementById("startBtn");
 
 var highScore = localStorage.getItem("highScore") || 0;
 var count = 0;
+var gameInterval; // will hold the collision check interval
 
-
-function jump(){
+function jump() {
     character.style.top = "110px";
-    setTimeout(function(){
-        character.style.top = "170px"   
-    },500);
-    
-    count++
+    setTimeout(function () {
+        character.style.top = "170px";
+    }, 500);
 
-    if(count > highScore){
-        highScore = count
-        localStorage.setItem("highScore", highScore)
+    count++;
+    if (count > highScore) {
+        highScore = count;
+        localStorage.setItem("highScore", highScore);
     }
-    document.getElementById("highScore").innerHTML = `highScore is  : ${highScore}`
-};
+    document.getElementById("highScore").innerHTML = `highScore is  : ${highScore}`;
+}
 
-window.addEventListener("keydown", jump);
-
-var block = document.getElementById("block");
-
-setInterval(function gameOver(){
+function gameOver() {
+    var block = document.getElementById("block");
     var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if((blockleft < 50) && (characterTop > 120)){
+    if (blockleft < 50 && characterTop > 120) {
+        clearInterval(gameInterval);
         result.style.display = "block";
         game.style.display = "none";
         score.innerHTML = `Score is : ${count}`;
-
     }
-},10);
+}
 
+function startGame() {
+    // Hide start button and show game area
+    if (startBtn) startBtn.style.display = "none";
+    if (game) game.style.display = "block";
+    // Attach jump listener
+    window.addEventListener("keydown", jump);
+    // Start collision detection loop
+    gameInterval = setInterval(gameOver, 10);
+}
 
-
+// Ensure result and game are hidden initially (handled via HTML inline styles)
